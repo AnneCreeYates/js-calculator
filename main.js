ab8851880f42da5a8800e3d151a37d9d00d05592
AnneC.Yates <annecreeyates@gmail.com>
2022-02-06 22:34:22 +0000

    removes 'onclick' from HTML; changes the selector for equality ti id; begins the clearBtn funtion logic - not complete yet

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-let outcome = document.querySelector('inputBox');
+//variables
+let answer = document.querySelector('.answer');
+let currentInput = document.querySelector('.currentInput');
+let buttons = document.querySelectorAll('buttons');
+let backspace = document.querySelector('#backspace');
+let clearBtn = document.querySelector('#clearBtn');
+let evaluate = document.querySelector('#equality');
 
-let buttons = document.querySelector('buttons');
+
+//Calculator display
+let actualDisplayedValue = [];
+
+
+//clear function
+clearBtn.addEventListener("click", () => {
+    actualDisplayedValue = [""];
+})
