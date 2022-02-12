3c0d6f84d0d6e0948a956a34b41545512a1c692d
AnneC.Yates <annecreeyates@gmail.com>
2022-02-12 19:06:44 +0000

    Changes a couple of selector names in HTML, but doesn't chenge the outcome - in CSS or HTML; adds Id to the table in HTML; adds button event listener in js - at the moment to see if it works at all - no specification of keys implemented yet

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 //variables
-let answer = document.querySelector('.answer');
-let currentInput = document.querySelector('.currentInput');
-let buttons = document.querySelectorAll('buttons');
-let backspace = document.querySelector('#backspace');
-let clearBtn = document.querySelector('#clearBtn');
-let evaluate = document.querySelector('#equality');
+let answer = document.querySelector(".answer");
+let currentInput = document.querySelector(".currentInput");
+// let numButtons = document.getElementsByClassName(".numButton");
+let funcButton = document.querySelectorAll(".funcButton");
+let backspace = document.getElementById("#backspace");
+let clearBtn = document.getElementById("#clearBtn");
+let evaluate = document.getElementById("#calculate");
 
 
 //Calculator display
 let actualDisplayedValue = [];
 
-
-//clear function
-clearBtn.addEventListener("click", () => {
-    actualDisplayedValue = [""];
-})
+document.addEventListener ("click", e => {
+  if (e.target.matches("button")) {
+      console.log("keys are working!")
+  }
+});
