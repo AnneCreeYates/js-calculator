af9aebdba01ed2a2b8e705e412070f57a896bd97
AnneC.Yates <annecreeyates@gmail.com>
2022-04-06 15:28:49 +0100

    Another version of the vanilla javascript calculator; It is to try out different apprach => HTML: changed from table to grid format, changed a bit the data usage - from descriptive data-type '...' to data-... -> makes the code clearer, changed Clear (C) to All Clear (AC), added different font; CSS: changed everything in CSS -> it is closer to the desired result, but still need a bit of work; JS was not changed at this stage yet

--- main.js
+++ main.js
@@ -6,12 +6,19 @@ const funcButton = calculator.getElementsByClassName(".funcButton");
 const backspace = document.getElementById("#backspace");
 const buttons = document.querySelectorAll("button");
 const keys = calculator.querySelector(".numPad-keys");
+let calcResult = 0;
+let firstNumber = 0;
+let secondNumber = 0;
 
 //body of the calculator functionality
-buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    if (button.value.match("button")) 
-        return
+//the loop goes from the beginning every time  a button is pressed and returns a specific outcome
+
+buttons.forEach((button) => 
+{
+  button.addEventListener("click", (event) => 
+  {
+    if (button.value.match("button"))
+        return;
 
         const key = event.target;
         const keyValue = key.value;
@@ -19,24 +26,29 @@ buttons.forEach((button) => {
         const { type } = key.dataset;
         const { previousKeyType } = calculator.dataset;
 
-
-      
-      // the below if statement makes it possible to use float numbers beginning with 0
-      if (type === "decimal")  {
         
-        if(displayValue === "0" 
-        || previousKeyType === "funcButton"
-        || previousKeyType === "numButton" ) {
+        
+      // the below if statement makes it possible to use float numbers beginning with 0
+      if (type === "decimal") 
+      {
+        
+        if (displayValue === "0" || 
+            previousKeyType === "funcButton" || 
+            previousKeyType === "numButton" 
+          ) 
+          {
           answer.textContent = displayValue + keyValue;
           // the if statement below checks if a decimal point exists in a number - if so it doesn't allow to use another one in the same number
           //it has to be here, otherwise it messes up the display value by not allowing 0.x after a function button if a number with decimal is on the display
-          if(displayValue.includes(".")){
+          if (displayValue.includes("."))
+          {
             answer.textContent = displayValue;
           }
         } 
         
-        if (previousKeyType ==="funcButton" && displayValue !== "0" 
-        || previousKeyType === "calculate") {
+        if (previousKeyType ==="funcButton" && displayValue !== "0" || 
+            previousKeyType === "calculate") 
+        {
           answer.textContent = "0" + keyValue;
         }
         
@@ -46,76 +58,94 @@ buttons.forEach((button) => {
       //(meaning the display shows 0) the keyValue replaces the
       // value on the display, if it is not the number is appended to the previous number
 
-      if (type === "numButton") {
-        if (displayValue === "0" || previousKeyType === "funcButton"  || previousKeyType === "calculate") {
+      if (type === "numButton") 
+      {
+        if (displayValue === "0" || 
+            previousKeyType === "funcButton"  || 
+            previousKeyType === "calculate") 
+        {
           answer.textContent = keyValue;
-        } else {
+        } 
+        else 
+        {
           answer.textContent = displayValue + keyValue;
         }
       }
 
-      if (type === "funcButton") {
-        
+      //pressing funcButton defines the firstNumber and activates the funcButton
+      if (type === "funcButton") 
+      {
         //this if statement makes the selected function button to be de-selected - change into function, because will be useful in several places
         const currentActiveFuncKey = calculator.querySelector('[data-state = "selected"]');
-        if (currentActiveFuncKey) {
+        if (currentActiveFuncKey) 
+        {
            currentActiveFuncKey.dataset.state = "";
+        }
 
-          }
         key.dataset.state = "selected";
         
         //the function below deselects the selected button - it has to stay here
-        document.getElementById("calculate").onclick = function() {
+        document.getElementById("calculate").onclick = function() 
+        {
           key.dataset.state = "";
         }
         
         //the below is to save the value of the first number and an operator before it is erased from the display
-        //figure out how to get the first an second number in a different way, because it would be need to be more 
-        //specific in the includes(".") lool 
+        //figure out how to get the first an second number in a different way, because it would be easier to use if it was a reusable function
         calculator.dataset.firstNumber = displayValue;
         calculator.dataset.funcButton = button.dataset.key;
-
-        console.log(calculator.dataset.firstNumber)
         
-
       }
-      
-         
-
-      // insert backspoace button logic here - lower the string is parsed to float
       
       //this is the logic for performing calculation
       //there is an issue with pressing the equal several times after entering the first and second number - the display shows for instance 1+2= 3 =4 =5 (adds the first numberto the output instead of the second)
-      if (type === "calculate") {
-         const firstNumber = calculator.dataset.firstNumber;
-         const funcButton = calculator.dataset.funcButton;
-         const secondNumber = displayValue;
+      
+      const firstNumber = calculator.dataset.firstNumber;
+      const funcButton = calculator.dataset.funcButton;
+      const secondNumber = displayValue;
 
-         answer.textContent = calculation (firstNumber, funcButton, secondNumber);
+      console.log("First Number: ", firstNumber);
+      console.log("Second number: ", secondNumber);
+
+      calcResult = calculation (firstNumber, funcButton, secondNumber);
+      console.log("Calcresult: ", calcResult);
+      if (type === "calculate" ) 
+      {
+
+        
+           answer.textContent = calcResult;
             
          //if the calculation does not contain a function button the disaplay is showing only the pressed number, but does not
          // perform any additional actions untill the function button and other numbers are pressed
-          if(!funcButton) {
+          if(!funcButton) 
+          {
             // console.log("there is no function button");
             answer.textContent = displayValue;
           }
+         
       }
 
-      if (type === "percent") {
+          
+
+      //the calculation should be performed after the the funcButton is being pressed second time 
+      
+      // if (type === "percent") {
         
-      }
+      // }
 
-      if (type === "backspace") {
+      if (type === "backspace") 
+      {
         const removed = displayValue.split('').slice(0, -1);
         answer.textContent = removed.join('');
 
-        if(removed.length === 0){
+        if(removed.length === 0)
+        {
           answer.textContent = "0";
         }
       }
 
-      if (type === "clearBtn") {
-        
+      if (type === "clearBtn") 
+      {
         answer.textContent = "0";
         delete calculator.dataset.firstNumber;
         delete calculator.dataset.funcButton;
@@ -131,17 +161,35 @@ buttons.forEach((button) => {
 
 // used functions
 
-// float may cause werid (broken) calculation result
-function calculation (firstNumber, funcButton, secondNumber) {
+// function performing calculation
+function calculation (firstNumber, funcButton, secondNumber) 
+{
   firstNumber = parseFloat(firstNumber);
   secondNumber = parseFloat(secondNumber);
+
+  // switch(funcButton) {
+  //   case "plus":
+  //       return firstNumber + secondNumber;
+  //       break;
+  //   case "minus":
+  //       return firstNumber - secondNumber;
+  //       break;
+  //   case "multiply":
+  //       return firstNumber * secondNumber;
+  //       break;
+  //   case "divide":
+  //       return firstNumber / secondNumber;
+  //       break;
+  //   default:
+  //     console.log("Nope")
+  // }
   if(funcButton === "plus") return firstNumber + secondNumber;
   if(funcButton === "minus") return firstNumber - secondNumber;
   if(funcButton === "multiply") return firstNumber * secondNumber;
   if(funcButton === "divide") return firstNumber / secondNumber;
   
-
 }
+
 
 
 //POTENTIAL EDGE CASES
