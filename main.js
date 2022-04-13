a9d35f1c00e8170465bd780d1c2c86961807d3da
AnneC.Yates <annecreeyates@gmail.com>
2022-04-13 15:13:34 +0100

    added the operation button sign to the dispay - was not visible before; added the getDisplayNumber functio, which allos for big number dividers - eg. 1,000.0 in English; updated the updateAnswer logic to accomodate the getDisplayNumber function

--- main.js
+++ main.js
@@ -27,13 +27,15 @@ class Calculator {
     
   }
 
+  
+
   mathOperation(funcButton) {
     if (this.currentDisplayedNum === '') return
     if(this.previousDisplayedNum !== '') {
       this.calculation();
     }
     this.funcButton = funcButton;
-    this.previousDisplayedNum = this.currentDisplayedNum;
+    this.previousDisplayedNum = this.currentDisplayedNum + this.funcButton;
     this.currentDisplayedNum= '';
     
   }
@@ -65,19 +67,57 @@ class Calculator {
     if(this.funcButton === '÷') computation = previous / current;
     //try changing the signs to names or entities from html
     
-    console.log(previous, current,this.funcButton, computation)
+    console.log(previous, this.funcButton, current, computation)
     this.currentDisplayedNum = computation;
     this.funcButton = undefined;
     this.previousDisplayedNum = '';
 
        
   }
+
+  //the function below makes it possible for the comma dividers to appear when dealing with bigger numbers
+  //it can be changed to any language
+  //it divides the decimal number into 2 sections - before and after the decimal point
+
+  getDisplayNumber(numButton) {
+    //turns the number to string
+    const strNum = numButton.toString();
+    //cuts off and stores the integer part of the number
+    const intNum = parseFloat(strNum.split('.')[0]);
+    //cuts off and stores the decimal part of the number
+    const decimalNum = strNum.split('.')[1];
+
+    let intDisplay;
+    if (isNaN(intNum)) {
+      intDisplay = '';
+    } else {
+      //the toLocaleString changes the notation to English in this case (1,000.0); maximumFractionDigits doesn't allow the deciml point to repeat more than once
+      intDisplay = intNum.toLocaleString('en',  {maximumFractionDigits: 0});
+    }
+
+    if (decimalNum != null) {
+      return `${intDisplay}.${decimalNum}`;
+    } else {
+      return intDisplay;
+    }
+  }
   
  
   updateAnswer() {
-    this.currentDisplayedNumValue.textContent = this.currentDisplayedNum;
-    this.previousDisplayedNumValue.textContent = this.previousDisplayedNum;
-    this.calculation();
+    //immediately below is previous logic
+    // this.currentDisplayedNumValue.textContent = this.currentDisplayedNum;
+    // this.previousDisplayedNumValue.textContent = this.previousDisplayedNum;
+    
+    // in the video here goes the  following (see if necessarily here- i added that functionality to the mathOperation function istead):
+    //it is making the getDisplayNumber function to be visible on the calculator display
+
+    this.currentDisplayedNumValue.textContent = this.getDisplayNumber(this.currentDisplayedNum)
+    
+    if (this.funcButton != null) {
+      this.previousDisplayedNumValue.textContent = this.previousDisplayedNum;
+    } else {
+      this.previousDisplayedNumValue.textContent = '';
+    }
   };
 
   
@@ -129,9 +169,6 @@ calculate.addEventListener('click', ()=> {
 
 //POTENTIAL EDGE CASES
 //1. the calculator doesn't allow to start with the 'minus' - beginning with the negative number not possible; clac. treats it as a normal number
-//2. it is not possible to add (or other function) more than 2 numbers together - after pressing the operator for the second time the actual secod number is overriden and only 2 numbers are added in the end
-//3. there is an issue with subtracting when pressing the equality several times - it doesn't subtract, just changes between first and second number -same case for division
-
 
 
 
